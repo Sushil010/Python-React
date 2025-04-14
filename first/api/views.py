f3e74b868b918019c039bf1bb013ba8088667b90
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-14 20:27:00 +0545

    view_changes

--- first/api/views.py
+++ first/api/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the first index.")
+    return HttpResponse("<h4>Hello, world. You're at the first index.</h4>")
