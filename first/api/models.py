e668eb32c65c5831fa5fd5b39fc97cb313c28bda
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-16 22:59:55 +0545

    model_defn

--- first/api/models.py
+++ first/api/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Add(models.Model):
-    pass
+    code=models.CharField(max_length=8,unique=True, default="")
+    host=models.CharField(max_length=10,unique=True)
+    guets_pause=models.BooleanField(null=False,default=False)
+    skip_vote=models.IntegerField(default=1,null=False)
+    created=models.DateTimeField(auto_now_add=True)
