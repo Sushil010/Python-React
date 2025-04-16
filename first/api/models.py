c62bc21b478b528844fe0d4005277a2230b58719
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-16 23:11:55 +0545

    randomo_code_mig

--- first/api/models.py
+++ first/api/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+import random
+import string
+
+
+def code_generator():
+    length=6
+    while True:
+        code=''.join(random.choices(string.ascii_uppercase(k=length)))
+         
+        # Add.objects.filter(code=code) returns a list that is if there is existing other then 
+        # count will increase else count 0 means there is not anything remaining.
+        
+        if Add.objects.filter(code=code).count()==0:
+            break
+    return code
+
 
 # Create your models here.
+
 class Add(models.Model):
     code=models.CharField(max_length=8,unique=True, default="")
     host=models.CharField(max_length=10,unique=True)
