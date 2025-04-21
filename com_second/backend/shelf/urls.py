4c7670e9ad173fe75b733707d4e1a3c17a6ecfc2
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-21 23:24:46 +0545

    back_comp

--- com_second/backend/shelf/urls.py
+++ com_second/backend/shelf/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
    path('getshelf/', views.getshelf, name='getshelf'),
    path('postshelf/', views.postshelf, name='postshelf'),
+   path('getshelf/<int:pk>/',views.updation,name='updation')
 ]
--- com_second/backend/shelf/views.py
+++ com_second/backend/shelf/views.py
@@ -23,3 +23,23 @@ def postshelf(request):
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['PUT','DELETE'])
+def updation(request,pk):
+    try:
+        shelf=Shelf.objects.get(pk=pk)
+    except Shelf.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method=="DELETE":
+        shelf.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method=="POST":
+        data=request.data
+        serializer=Shelfserializer(shelf,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
