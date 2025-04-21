0d93daabdcad5260667f60dc057375df9fe21960
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-21 22:06:21 +0545

    layout_init

--- com_second/frontend/src/componenets/Home.jsx
+++ com_second/frontend/src/componenets/Home.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-2xl'>
+    <div className=''>
+        
+        <div className=''>
+
+        <input className='border border-black p-1.5 ml-3' type="text" placeholder='Enter Name..'/>
+        <input className='border border-black ml-3 p-1.5' type="text" placeholder='Enter Date...' />
+        
+        <button className='border active:scale-90 cursor-pointer ml-3 border-white text-yellow-400 text-2xl p-1'>
+          Submit
+        </button>
         
         
-        Home
+
+        </div>
+        
+      
     
     </div>
   )
