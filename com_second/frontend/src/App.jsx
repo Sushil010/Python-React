38ca8f2df594e024e063bcdb50a1662bbedc32b0
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-21 21:39:37 +0545

    client_setup

---
+++ com_second/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './componenets/Home'
+
+const App = () => {
+  
+  return (
+    <div className='bg-gray-700 text-white w-full h-[100vh]'>
+      
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App
