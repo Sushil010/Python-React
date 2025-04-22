db46a2f700689ee1fe1aebbf57ba4853a181bad6
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-22 14:16:57 +0545

    submit_update

--- com_second/frontend/src/componenets/Home.jsx
+++ com_second/frontend/src/componenets/Home.jsx
@@ -15,6 +15,43 @@ const Home = () => {
     setShelf(data)
   }
 
+  const putvalue=async()=>{
+
+    const shelfdata={
+      // frontend variables:backend varaibales
+      // name:name,
+      // date:date
+      name,
+      date
+    }
+
+    try {
+
+      const dpost = await fetch ("http://127.0.0.1:8000/postshelf/",{
+
+        method:"POST",
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(shelfdata)
+      })
+
+      const data=await dpost.json()
+      setShelf((prev)=>[...prev,data])
+
+
+    } 
+    
+    
+    catch (error) {
+      
+      console.log(error)
+
+    }
+    
+    
+  }
+
   useEffect(() => {
     
     getvalue()
@@ -47,6 +84,7 @@ const Home = () => {
 
 
         <button 
+        onClick={putvalue}
         className='border active:scale-90 cursor-pointer ml-3
          border-white text-yellow-400 text-2xl p-1'>
           Submit
@@ -56,8 +94,14 @@ const Home = () => {
 
           {shelf.map(function(value,idx){
             return <div key={idx} className=''>
-                <h4 className='text-yellow-500 ml-3'>Title: {value.name}</h4>
-                <h4 className='text-blue-500 mb-2 ml-3'>Released Year:{value.date}</h4>
+                <h4 className='text-yellow-500 text-2xl mt-2 ml-3'>Title: {value.name}</h4>
+                <h4 className='text-blue-500 mb-2 text-2xl ml-3'>Released Year:{value.date}</h4>
+                <button 
+                className='text-red-600  cursor-pointer 
+                text-2xl ml-3 p-1.5 active:scale-90 border border-amber-100'>
+                  Delete
+                </button>
+                <hr className='border-b border-gray-300 my-4' />
             </div>
           })}
         
