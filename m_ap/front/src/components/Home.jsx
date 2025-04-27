2d77a3635c2a395154929e1c75f3bbb782f140fc
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-28 00:17:28 +0545

    fetch_loop

--- m_ap/front/src/components/Home.jsx
+++ m_ap/front/src/components/Home.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
 
-    const [dat, setDat] = useState([])
-
+    const [dat, setDat] = useState({})
+    useEffect(() => {
+    
+    datas()
+     
+    }, [])
+    
     const datas=async()=>{
         const response =await fetch (" http://www.omdbapi.com/?i=tt3896198&apikey=48c44e9e")
         const values= await response.json()
@@ -16,16 +21,36 @@ const Home = () => {
     <div className=' text-white text-2xl w-full h-[100vh] bg-gray-700'>
 
    
-    <button
+    {/* <button
     onClick={datas}
     className='p-1.5 mx-1 my-5 border border-black text-green-500 cursor-pointer active:scale-90'
     >
         Click
-    </button>
+    </button> */}
 
 
-    {dat.map(
-        
+    {/* {dat.map(function(value,idx){
+        return <div key={idx}>
+            <img src="" alt="" />
+            <h1>{value.Title}</h1>
+        </div>
+    })
+    
+    
+    } */}
+
+
+    {dat.Title && (
+        <div>
+            <img src={dat.Poster} alt="" />
+            <h4>{dat.Title}</h4>
+            {/* <h5>{dat.Ratings[0].Value}</h5> */}
+            {dat.Ratings && dat.Ratings.map(function(values,idx){
+                return <div key={idx}>
+                        <h5>{values.Source}:{values.Value}</h5>
+                </div>
+            })}
+        </div>
     )}
 
 
