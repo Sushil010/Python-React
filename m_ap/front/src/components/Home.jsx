958451ddbbea4c8b52952bfaf5eacca851521453
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-30 23:58:20 +0545

    loader_comp

--- m_ap/front/src/components/Home.jsx
+++ m_ap/front/src/components/Home.jsx
@@ -4,22 +4,34 @@ const Home = () => {
     // const apiKey="48c44e9e"
     const [dat, setDat] = useState([])
     const [search, setSearch] = useState("")
+    const [loader, setLoader] = useState(false)
     
     useEffect(() => {
+    if(search.trim()!=""){
+        datas()
+    }
     
-    datas()
      
-    }, [])
+    }, [search])
 
 
     // http://www.omdbapi.com/?s=Batman&page=2
     // http://www.omdbapi.com/?s=batman&page=2&apikey=yourkey
     
     const datas=async()=>{
-        const response =await fetch ("http://www.omdbapi.com/?s=batman&page=2&apikey=48c44e9e")
-        const values= await response.json()
-        console.log(values)
-        setDat(values.Search)
+        setLoader(true)
+        try {
+            const response =await fetch (`http://www.omdbapi.com/?s=${search}&page=2&apikey=48c44e9e`)
+            const values= await response.json()
+            console.log(values)
+            setDat(values.Search)
+        } catch (error) {
+            console.log(error)
+            setDat([])
+        }finally{
+            setLoader(false)
+        }
+        
     }
 
 
@@ -29,25 +41,38 @@ const Home = () => {
     <div className='flex justify-center items-center'>
         <input 
         onChange={(e)=>{setSearch(e.target.value)}}
+        
         type="text" 
         placeholder='Search Movies'
-        className='my-5'
+        className='my-5 border-green-400 border p-3 '
         
         />
     </div>
    
     
+    {loader?(
+        <div className='text-center text-xl py-10'>loading...</div>
+        ): (
+        <div  className='grid grid-cols-3'>
+        {dat?.map(function(value,index){
+            return <div className='flex flex-col text-center p-4 rounded-2xl items-center justify-center mb-3'
+            key={index}>
+                
+                <img src={value.Poster} alt={value.Title} />
+                <h5>
+                    {value.Title}<br/>
+                    {value.Year}
+                </h5>
 
-{/* 
-    {dat.map(function(value,idx){
-        return <div key={idx}>
-            <img src="" alt="" />
-            <h1>{value.Title}</h1>
-        </div>
-    })
-    
-    
-    } */}
+            </div>
+        })}
+
+    </div>
+        )
+    }
+   
+ 
+
 
 
     {/* {dat.Title && (
@@ -63,8 +88,10 @@ const Home = () => {
         </div>
     )} */}
 
-        <div  className='grid grid-cols-3'>
-    {dat.map(function(value,index){
+        {/* <div  className='grid grid-cols-3'>
+    {dat.
+    filter(item=>item.Title.toLowerCase().includes(search.toLowerCase()))
+    .map(function(value,index){
         return <div className='flex flex-col text-center p-4 rounded-2xl items-center justify-center mb-3'
          key={index}>
             
@@ -78,7 +105,7 @@ const Home = () => {
     })}
 
         </div>
-
+ */}
 
 
     </div>
