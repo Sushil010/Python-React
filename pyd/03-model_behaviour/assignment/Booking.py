011c3c3176ebfde98c3cac40e663b5c370748a6b
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-06 19:53:27 +0545

    Field_transform

--- pyd/03-model_behaviour/assignment/Booking.py
+++ pyd/03-model_behaviour/assignment/Booking.py
@@ -1,18 +1,18 @@
-from pydantic import BaseModel,field_validator,model_validator,computed_field #type:ignore
+from pydantic import BaseModel,Field,field_validator,model_validator,computed_field #type:ignore
 
 
 
 class Booking(BaseModel):
     user_id:int
     room_id:int
-    nights:int
+    nights:int=Field(...,ge=1)
     rate_per_night:float
 
-    @field_validator
-    def nt_valid(cls,v):
-        if(v<1):
-            raise ValueError("Invalid night value")
-        return v
+    # @field_validator
+    # def nt_valid(cls,v):
+    #     if(v<1):
+    #         raise ValueError("Invalid night value")
+    #     return v
     
     @computed_field
     @property
