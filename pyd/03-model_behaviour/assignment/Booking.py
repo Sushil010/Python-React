60a8d40b866ec1af1f9d75e0abd111cfd0f6b08b
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-06 19:48:56 +0545

    booking_model

---
+++ pyd/03-model_behaviour/assignment/Booking.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel,field_validator,model_validator,computed_field #type:ignore
+
+
+
+class Booking(BaseModel):
+    user_id:int
+    room_id:int
+    nights:int
+    rate_per_night:float
+
+    @field_validator
+    def nt_valid(cls,v):
+        if(v<1):
+            raise ValueError("Invalid night value")
+        return v
+    
+    @computed_field
+    @property
+
+    def total_amt(self)->float:
+        return self.nights*self.rate_per_night
