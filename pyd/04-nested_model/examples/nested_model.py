ee57f1520d2aba2e12f2720a977d31b517d70085
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-07 23:15:15 +0545

    nested_module

--- pyd/04-nested_model/examples/nested_model.py
+++ pyd/04-nested_model/examples/nested_model.py
@@ -13,3 +13,12 @@ class User(BaseModel):
     # Address will have the structure as defined above in Address class
     address:Address
 
+class Comment(BaseModel):
+    id:int
+    name:str
+    # Looks like this structure 
+    # replies->optional->list[[id,name,replies],[id,name,replies].......]
+    replies:Optional[List['Comment']]=None
+
+
+Comment.model_rebuild()
