df12b5fe08e621e7ac32e8592f3a70f83e1556c7
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-07 23:11:17 +0545

    call_other_module

--- pyd/04-nested_model/examples/nested_model.py
+++ pyd/04-nested_model/examples/nested_model.py
@@ -1,3 +1,15 @@
 from pydantic import BaseModel #type:ignore
-from typing import List
+from typing import List,Optional
+
+
+class Address(BaseModel):
+    id:int
+    location:str
+    postal_code:str
+
+class User(BaseModel):
+    id:int
+    name:str
+    # Address will have the structure as defined above in Address class
+    address:Address
 
