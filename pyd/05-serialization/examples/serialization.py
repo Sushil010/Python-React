1ca499a7e4b8088b947fc8a50faefb111f189da9
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-08 23:30:05 +0545

    mode_defn

--- pyd/05-serialization/examples/serialization.py
+++ pyd/05-serialization/examples/serialization.py
@@ -1,2 +1,21 @@
 from pydantic import BaseModel #type:ignore
 from typing import Lists
+from datetime import datetime
+
+
+
+class Address(BaseModel):
+   street:str
+   city:str
+   zipcode:str
+
+
+class User(BaseModel):
+   id:int
+   name:str
+   isActive:bool=True
+   email:str
+   address:Address
+   datetime:datetime
+   tags:Lists[str]=[]
+
