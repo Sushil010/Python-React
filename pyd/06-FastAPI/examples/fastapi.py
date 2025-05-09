0ab4e3da44f8dcb5ec63a51500335dc20a214955
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-09 22:44:14 +0545

    model_defn

--- pyd/06-FastAPI/examples/fastapi.py
+++ pyd/06-FastAPI/examples/fastapi.py
@@ -5,3 +5,13 @@ from pydantic import BaseModel, EmailStr #type:ignore
 app=FastAPI()
 
 
+
+class User(BaseModel):
+    name:str
+    Email:EmailStr
+    password:str
+
+
+class Settings(BaseModel):
+    app_name:str="App Test"
+    admin_mail:str="admin@gmail.com"
