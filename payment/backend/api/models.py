5450776947dd1b17c31e55c7ab0a0fc4e176be61
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-11 23:04:47 +0545

    db_and_table_design

--- payment/backend/api/models.py
+++ payment/backend/api/models.py
@@ -7,18 +7,31 @@
 # from django.db import models
 from pydantic import BaseModel,Field#type:ignore
 from fastapi import FastAPI #type:ignore
-from typing import Literal
-from sqlmodel import SQLModel,create_engine,Session
+from typing import Literal,Optional
+from sqlmodel import SQLModel,create_engine,Session #type:ignore
 
 
 app=FastAPI()
 # Create your models here.
 class PaymentRequest(SQLModel,table=True):
+    id:Optional[int]=Field(primary_Key=True)
     amount:float
     currency:Literal['NPR','USD']=Field(...,description='3-letter currency code')
     source:Literal['esewa','Khalti']=Field(...,description='Payment Provider')
     remarks:str=Field(...,min_length=3,max_length=200,description='Min 3 letter required')
 
+#Define databse name 
+file_name="database.db"
+engine=create_engine(f"sqlite:///{file_name}",echo=True)
+
+#Create database table
+def create_table():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+def onstartup():
+    create_table()
 
 @app.get("/")
 def read_root():
