59e4da46dd0e0793925f782d897433d42cad15c1
Sushil Sharma <sharma.s420823@gmail.com>
2025-05-11 15:54:09 +0545

    payment_method_model

--- payment/backend/api/models.py
+++ payment/backend/api/models.py
@@ -1,7 +1,25 @@
 # from django.db import models
-from pydantic import BaseModel, Field
+from pydantic import BaseModel,Field#type:ignore
+from fastapi import FastAPI #type:ignore
+from typing import Literal
 
 
+app=FastAPI()
 # Create your models here.
 class PaymentRequest(BaseModel):
-    amount:
+    amount:float
+    currency:Literal['NPR','USD']=Field(...,description='3-letter currency code')
+    source:Literal['esewa','Khalti']=Field(...,description='Payment Provider')
+    remarks:str=Field(...,min_length=3,max_length=200,description='Min 3 letter required')
+
+@app.post("/payments")
+def creatrepayment(payment:PaymentRequest):
+    return {"status":"success", "data":payment}
+# users=PaymentRequest(
+#     amount=42323.12,
+#     currency='NPR',
+#     source='Khalti',
+#     remarks='pay'
+# )
+
+# print(users)
